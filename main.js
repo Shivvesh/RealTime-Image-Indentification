103cbcbeda4915a77ae10b23356e12cc3be45281
Shivvesh <71574601+Shivvesh@users.noreply.github.com>
2021-07-30 15:33:19 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -3,25 +3,28 @@ function setup() {
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
-    classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/v_sl95BzE/model.json',modelLoaded);
-  }
-  
-  function modelLoaded() {
-      console.log('Model Loaded!');
-  }
-  
-  function draw() {
+    classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/RhNbs7ZNI/model.json'),modelLoaded
+}
+
+function modelLoaded()
+{
+    console.log('Model Loaded!');
+}
+
+function draw()
+{
     image(video, 0, 0, 300, 300);
-    classifier.classify(video, gotResult);
-  }
-  
-  function gotResult(error, results) {
-    if (error) {
-      console.error(error);
+    classifier.classify(video, gotResults);
+}
+
+function gotResults(error, results)
+{
+    if (error)
+    {
+        console.error(error);
     } else {
-      console.log(results);
-      document.getElementById("result_object_name").innerHTML = results[0].label;
-      document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    console.log(results);
+    document.getElementById("result_object_name").innerHTML = results[0].label;
+    document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
     }
-  }
-  
+}
